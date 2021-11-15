06365089285efa7029b75249f8882fa40209207c
Tim Nilsson <tim.r.nilsson@gmail.com>
2021-11-15 11:13:57 +0100

    Created the basic setup layout

---
+++ script.js
@@ -0,0 +1,4 @@
+const btnNewGameEl = document.querySelector('#btn-new-game');
+const roundTitleEl = document.querySelector('#round-title');
+const roundListEl = document.querySelector('#round-list');
+const btnGuessEl = document.querySelector('#btn-guess');
